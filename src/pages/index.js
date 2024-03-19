ff5075a7f2a1b021660b597d12730d2e9f0f86f9
atareversei (pc) <faraji.ata92@gmail.com>
2024-03-19 15:13:46 +0330

    add assets + index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import s from './index.module.css'
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
@@ -8,6 +10,16 @@ export default function Home() {
       title={`Home`}
       description="Qwest Docs holds the reports, issues, milestones, ideas, dreams, and software documentation.">
       <main>
+        <section className={`${s.hero} ${s.section}`}>
+          <figure className={s.figure}>
+            <img src='img/logo.png' />
+          </figure>
+          <div className={s.content}>
+            <h1>Qwest</h1>
+            <p>The simplest project management tool ever known to mankind</p>
+          </div>
+          <Link className={s.specialLink} to='/docs/preliminary/intro'>Get Started with Qwest 🤙</Link>
+        </section>
       </main>
     </Layout>
   );
